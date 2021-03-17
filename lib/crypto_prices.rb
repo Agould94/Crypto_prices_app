09d8c60388f008267213688e28533ee167d044d3
Alexander Gould <alexander.gould94@gmail.com>
2021-03-17 12:45:06 -0700

    added changes to api and main file

--- lib/crypto_prices.rb
+++ lib/crypto_prices.rb
@@ -1,12 +1,34 @@
 # frozen_string_literal: true
 
 require_relative "crypto_prices/version"
+require_relative "crypto_prices/nomics_api"
 
 module CryptoPrices
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Cryptocurrency
+    attr_accessor :name, :current_price, :available_markets
+  
+    @@all = []
+
+    def initialize(name=nil, current_price=0, available_markets=nil)
+      @name = name
+      @current_price = current_price
+      @available_markets = available_markers
+    end
+
+
+  end
+
+
+
+
+
+
+
+
+
+
 end
 
 
-class Crytocurrency
-end
--- lib/nomics_api.rb
+++ lib/nomics_api.rb
@@ -1,15 +1,44 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
-class GetPrograms
-    URL = "https://api.nomics.com/v1/markets?key=421c2246c0e25e7aad14db3397041634"
+require 'pry'
 
-    def get_programs
-        uri = URI.parse(URL)
+class GetPrograms
+    URLM = "https://api.nomics.com/v1/markets?key=421c2246c0e25e7aad14db3397041634"
+    URLC = "https://api.nomics.com/v1/currencies/ticker?key=421c2246c0e25e7aad14db3397041634&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1"
+
+
+    def get_programs_for_markets
+        uri = URI.parse(URLM)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def get_programs_for_currencies
+        uri = URI.parse(URLC)
         response = Net::HTTP.get_response(uri)
         response.body
     end
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+market_programs = GetPrograms.new.get_programs_for_markets
+currency_programs = GetPrograms.new.get_programs_for_currencies
+
+parsed = JSON.parse(currency_programs)
+
+def currency_ids(input)
+    input.each do |currency|
+        puts currency["id"]
+    end
+end
+
+def currency_names(input)
+    input.each do |currency|
+        puts currency["name"]
+    end
+end
+
+puts currency_ids(parsed)
+
+#binding.pry
+#p parsed[][][]
