3b5ea550d05d2814eb2ea670898a7efff84e18f8
Alexander Gould <alexander.gould94@gmail.com>
2021-03-16 20:22:59 -0700

    first commit

---
+++ lib/crypto_prices.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "crypto_prices/version"
+
+module CryptoPrices
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/crypto_prices/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CryptoPrices
+  VERSION = "0.1.0"
+end
