288f161cca64543b6d4beb58ad492be1f9edd2ed
Alexander Gould <alexander.gould94@gmail.com>
2021-03-18 14:53:30 -0700

    added the ability to get a currency by its id name in currency class

--- lib/crypto_prices/Currencies.rb
+++ lib/crypto_prices/Currencies.rb
@@ -1,5 +1,11 @@
-class Currencies
-    def initialize(hash)
+class Currency
+    attr_reader :hash
+    attr_accessor :id, :currency, :name, :status, :price, :date, :supply
+
+    @@all = []
+
+    def initialize(coin)
+        hash = CryptoData.new.get_currency(coin)
         @id = hash["id"]
         @currency = hash["currency"]
         @name = hash["name"]
@@ -7,10 +13,12 @@ class Currencies
         @price = hash["price"]
         @date = hash["price_date"]
         @supply = hash["circulating_supply"]
-
+        @@all << self
     end
 
-    
+    def self.all
+        @@all
+    end
 
     # def currency_ids(input)
 #     input.each do |currency|
