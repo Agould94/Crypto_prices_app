82def5c42a75b30169bba9883881609b54cf966e
Alexander Gould <alexander.gould94@gmail.com>
2021-03-18 19:35:22 -0700

    added several new variables andprint details method

--- lib/crypto_prices/Currencies.rb
+++ lib/crypto_prices/Currencies.rb
@@ -1,11 +1,10 @@
 class Currency
     attr_reader :hash
-    attr_accessor :id, :currency, :name, :status, :price, :date, :supply
+    attr_accessor :id, :currency, :name, :status, :price, :date, :supply, :market_cap, :num_exchanges, :high, :high_timestamp
 
     @@all = []
 
     def initialize(hash)
-        #hash = CryptoData.new.get_currency(coin)
         @id = hash["id"]
         @currency = hash["currency"]
         @name = hash["name"]
@@ -13,6 +12,10 @@ class Currency
         @price = hash["price"]
         @date = hash["price_date"]
         @supply = hash["circulating_supply"]
+        @market_cap = hash["market_cap"]
+        @num_exchanges = hash["num_exchanges"]
+        @high = hash["high"]
+        @high_timestamp = hash["high_timestamp"]
         @@all << self
     end
 
@@ -24,22 +27,15 @@ class Currency
         self.all.find{|coin| coin.name == name.capitalize}
     end
 
-    # def currency_ids(input)
-#     input.each do |currency|
-#         puts currency["id"]
-#     end
-# end
+    def print_details
+        puts self.name
+        puts "Current_price: #{self.price}"
+        puts "Symbol: #{self.id}"
+        puts "Market Capitalization: #{self.market_cap}"
+        puts "This currency is available on #{self.num_exchanges} different exchanges."
 
-# def currency_names(input)
-#     input.each do |currency|
-#         puts currency["name"]
-#     end
-# end
+    end
 
-#build a class
-# 
-#binding.pry
-#p parsed[][][]
 end
 
 
